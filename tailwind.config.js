349a912cee6a3ec3e32e5c9a5944df0d074db184
colbery <92644519+colbery@users.noreply.github.com>
2023-06-02 22:29:05 +0200

    nextUpdate

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+      boxShadow: {
+        '10xl': 'inset -10px 18px 36px -18px #000000;',
+        '9xl': '0 12px 13px -5px rgba(0, 0, 0, 0.9)',
+      }
+    }
   },
   plugins: [],
 }
