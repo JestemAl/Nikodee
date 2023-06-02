165210ac1e3b911ee36f663d5890ce79c902ac78
colbery <92644519+colbery@users.noreply.github.com>
2023-06-02 23:01:33 +0200

    nextUpdate

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ export default {
       boxShadow: {
         '10xl': 'inset -10px 18px 36px -18px #000000;',
         '9xl': '0 12px 13px -5px rgba(0, 0, 0, 0.9)',
-      }
+      },
     }
   },
   plugins: [],
