66d2e308a6c2dffe05cb39d20ece32360fbdcb73
colbery <92644519+colbery@users.noreply.github.com>
2023-06-03 14:48:44 +0200

    nextUpdate

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,16 @@ export default {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
       },
       boxShadow: {
+        '11xl': 'inset 0px 30px 60px -15px #000000',
         '10xl': 'inset -10px 18px 36px -18px #000000;',
         '9xl': '0 12px 13px -5px rgba(0, 0, 0, 0.9)',
       },
       fontFamily: {
         body: ['Inter', 'sans-serif'],
         inria: ['Inria Serif', 'sans-serif']
+      },
+      borderRadius: {
+        '2xl': '1.1rem'
       },
     }
   },
