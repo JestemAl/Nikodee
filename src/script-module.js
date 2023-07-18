458b0add0db4f51a726dc680bcb87071ab31a4bc
JestemAl <aleksander.pietrzak777@gmail.com>
2023-07-18 11:57:22 +0200

    update js

--- src/script-module.js
+++ src/script-module.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
 // import * as dat from 'lil-gui'
-// import gsap from 'gsap'
+import gsap from 'gsap'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
@@ -8,9 +8,7 @@ import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 /**
  * params
  */
-const objectsDistance = 4
-const panelPosition = 1
-const modulePosition = -1 
+THREE.ColorManagement.enabled = false
 
 /**
  * Base
@@ -21,7 +19,7 @@ const modulePosition = -1
 // })
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector('canvas.webgl-module')
 
 // Scene
 const scene = new THREE.Scene()
@@ -44,18 +42,12 @@ gltfLoader.setDRACOLoader(dracoLoader)
  * Textures
  */
 const moduleTexture = textureLoader.load('models/module/moduleBaked.jpg')
-moduleTexture.colorSpace = THREE.SRGBColorSpace
-
-const panelTexture = textureLoader.load('models/panel/view-from-camera/panelBaked.jpg')
-panelTexture.colorSpace = THREE.SRGBColorSpace
+// moduleTexture.colorSpace = THREE.SRGBColorSpace
 
 /**
  * Materials
  */
 // Baked material
-const panelMaterial = new THREE.MeshBasicMaterial({ map: panelTexture })
-panelTexture.flipY = false
-
 const moduleMaterial = new THREE.MeshBasicMaterial({ map: moduleTexture })
 moduleTexture.flipY = false
 
@@ -63,43 +55,19 @@ moduleTexture.flipY = false
 /**
  * Models
  */
-// Panel
-const models = []
-
-var panelModel 
-gltfLoader.load(
-    'models/panel/panel.glb',
-    (gltf) =>
-    {
-        panelModel = gltf.scene
-
-        panelModel.traverse((child) => 
-        {
-            child.material = panelMaterial
-        })
-        panelModel.scale.set(0.14 * (sizes.width / sizes.height), 0.14 * (sizes.width / sizes.height), 0.14 * (sizes.width / sizes.height))
-        panelModel.position.y = panelPosition
-        panelModel.rotation.set(1.4, 0, 0)
-
-        scene.add(panelModel)
-    }
-)
-
-// Module
 var moduleModel
 gltfLoader.load(
     'models/module/module.glb',
     (gltf) =>
     {
         moduleModel = gltf.scene
-        models.push(moduleModel)
 
         moduleModel.traverse((child) => 
         {
             child.material = moduleMaterial
         })
         moduleModel.scale.set(0.3 * (sizes.width / sizes.height), 0.3 * (sizes.width / sizes.height), 0.3 * (sizes.width / sizes.height))
-        moduleModel.position.y = modulePosition
+        // moduleModel.position.y = modulePosition
 
         // modulePosition = moduleModel.position.y
 
@@ -110,28 +78,26 @@ gltfLoader.load(
 /**
  * Lights
  */
-const directionalLight = new THREE.DirectionalLight('#ffffff', 1)
-directionalLight.position.set(1, 1, 0)
-scene.add(directionalLight)
+// const directionalLight = new THREE.DirectionalLight('#ffffff', 2)
+// directionalLight.position.set(1, 1, 0)
+// scene.add(directionalLight)
 
 /**
  * Sizes
  */
 const sizes = {
     width: window.innerWidth,
-    height: 2000
+    height: window.innerHeight
 }
 
 window.addEventListener('resize', () =>
 {
     // Update models
-    panelModel.scale.set(0.14 * (sizes.width / sizes.height), 0.14 * (sizes.width / sizes.height), 0.14 * (sizes.width / sizes.height))
     moduleModel.scale.set(0.3 * (sizes.width / sizes.height), 0.3 * (sizes.width / sizes.height), 0.3 * (sizes.width / sizes.height))
 
-    
     // Update sizes
     sizes.width = window.innerWidth
-    sizes.height = 2000
+    sizes.height = window.innerHeight
 
     // Update camera
     camera.aspect = sizes.width / sizes.height
@@ -155,42 +121,45 @@ const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 
 camera.position.z = 6
 cameraGroup.add(camera)
 
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
+// controls
+const controls = new OrbitControls(camera, canvas)
+controls.target.set(0, 0, 0)
+controls.enableDamping = true
+controls.enableZoom = false
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
-    alpha: true
+    alpha: true,
+    antialias: true
 })
+renderer.useLegacyLights = false
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-renderer.outputColorSpace = THREE.SRGBColorSpace;
+// renderer.outputColorSpace = THREE.SRGBColorSpace;
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace
 
 /**
  * Scroll
  */
 let scrollY = window.scrollY
-let currentSection = 0
-
 
 window.addEventListener('scroll', () =>
 {
     scrollY = window.scrollY
     const scrollPosition = scrollY / sizes.height
 
-    if(models)
-    {
-        if(scrollPosition >= 2.45 && scrollPosition <= 2.83)
-        {
-            moduleModel.rotation.y = (scrollPosition - 2.45) * 9
-            moduleModel.position.y = - (scrollPosition * 1.4 - 2.2)
-        }
-        console.log(- (scrollPosition * 1.4 - 2.3));
-    }
+    // if(moduleModel)
+    // {
+    //     // console.log(scrollPosition);
+    //     if(scrollPosition >= 3.5 && scrollPosition <= 4.3)
+    //     {
+    //         moduleModel.rotation.y = (scrollPosition - 2.45) * 9
+    //         // moduleModel.position.y = - (scrollPosition * 1.4 - 2.2)
+    //     }
+    // }
 })
 
 
@@ -224,10 +193,10 @@ const tick = () =>
     const parallaxY = - cursor.y * 0.05
     cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
     cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
-
-    // // Update controls
-    // controls.update()
     
+    // Update controlls
+    controls.update()
+
     // Render
     renderer.render(scene, camera)
 
---
+++ src/script-panel.js
@@ -0,0 +1,217 @@
+import * as THREE from 'three'
+// import * as dat from 'lil-gui'
+import gsap from 'gsap'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+
+/**
+ * Base
+ */
+THREE.ColorManagement.enabled = false
+// Debug
+// const gui = new dat.GUI({
+//     width: 400
+// })
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl-panel')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Loaders
+ */
+// Texture loader
+const textureLoader = new THREE.TextureLoader()
+
+// Draco loader
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('draco/')
+
+// GLTF loader
+const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
+
+/**
+ * Textures
+ */
+const panelTexture = textureLoader.load('models/panel/view-from-camera/panelBaked.jpg')
+panelTexture.colorSpace = THREE.SRGBColorSpace
+
+/**
+ * Materials
+ */
+// Baked material
+const panelMaterial = new THREE.MeshBasicMaterial({ map: panelTexture })
+panelTexture.flipY = false
+
+/**
+ * Models
+ */
+// Panel
+var panelModel 
+gltfLoader.load(
+    'models/panel/panel.glb',
+    (gltf) =>
+    {
+        panelModel = gltf.scene
+
+        panelModel.traverse((child) => 
+        {
+            child.material = panelMaterial
+        })
+        panelModel.scale.set(0.14 * (sizes.width / sizes.height), 0.14 * (sizes.width / sizes.height), 0.14 * (sizes.width / sizes.height))
+        panelModel.rotation.set(1.4, 0, 0)
+        panelModel.position.y = -.5
+
+        scene.add(panelModel)
+    }
+)
+
+/**
+ * Lights
+ */
+// const directionalLight = new THREE.DirectionalLight('#ffffff', 1)
+// directionalLight.position.set(1, 1, 0)
+// scene.add(directionalLight)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update models
+    panelModel.scale.set(0.14 * (sizes.width / sizes.height), 0.14 * (sizes.width / sizes.height), 0.14 * (sizes.width / sizes.height))
+
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+
+/**
+ * Camera
+ */
+// Group
+const cameraGroup = new THREE.Group()
+scene.add(cameraGroup)
+
+// Base camera
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 6
+cameraGroup.add(camera)
+
+// controls
+const controls = new OrbitControls(camera, canvas)
+controls.target.set(0, 0, 0)
+controls.enableDamping = true
+controls.enableZoom = false
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    alpha: true,
+    antialias: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+// renderer.outputColorSpace = THREE.SRGBColorSpace;
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace
+
+/**
+ * Scroll
+ */
+let scrollY = window.scrollY
+let currentSection = 0
+
+
+window.addEventListener('scroll', () =>
+{
+    scrollY = window.scrollY
+    const scrollPosition = scrollY / sizes.height
+    const newSection = Math.round(scrollY / window.innerHeight)
+    const panelSection = 7
+
+
+    if(panelModel)
+    {
+        console.log(panelModel.position.y);
+        if(newSection != currentSection)
+        {
+            currentSection = newSection
+
+            if(currentSection == panelSection)
+            {
+                gsap.to(
+                    panelModel.rotation,
+                    {
+                        duration: 1.5,
+                        ease: 'power2.inOut',
+                        z: Math.PI * 2,
+                    }
+                )
+            }  
+        }
+    }
+})
+
+
+
+/**
+ * Cursor
+ */
+const cursor = {}
+cursor.x = 0
+cursor.y = 0
+
+window.addEventListener('mousemove', (event) =>
+{
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = event.clientY / sizes.height - 0.5
+})
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+let previousTime = 0
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
+
+    const parallaxX = cursor.x * 0.05
+    const parallaxY = - cursor.y * 0.05
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
+    
+    // Controls
+    controls.update()
+    
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
