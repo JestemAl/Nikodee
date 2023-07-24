3a785ccefc47870c75c7b0dc687640ede2bf2ec4
JestemAl <aleksander.pietrzak777@gmail.com>
2023-07-24 19:39:40 +0200

    update js

--- src/script-module.js
+++ src/script-module.js
@@ -21,10 +21,13 @@ const gui = new dat.GUI({
 })
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl-module')
+const canvasModule = document.getElementById('webgl-module')
+const canvasPanel = document.getElementById('webgl-panel')
+
 
 // Scene
-const scene = new THREE.Scene()
+const scenePanel = new THREE.Scene()
+const sceneModule = new THREE.Scene()
 
 /**
  * Loaders
@@ -46,10 +49,16 @@ gltfLoader.setDRACOLoader(dracoLoader)
 const moduleTexture = textureLoader.load('models/module/moduleBaked.jpg')
 // moduleTexture.colorSpace = THREE.SRGBColorSpace
 
+const panelTexture = textureLoader.load('models/panel/view-from-camera/panelBaked.jpg')
+// panelTexture.colorSpace = THREE.SRGBColorSpace
+
 /**
  * Materials
  */
 // Baked material
+const panelMaterial = new THREE.MeshBasicMaterial({ map: panelTexture })
+panelTexture.flipY = false
+
 const moduleMaterial = new THREE.MeshBasicMaterial({ map: moduleTexture })
 moduleTexture.flipY = false
 
@@ -57,6 +66,30 @@ moduleTexture.flipY = false
 /**
  * Models
  */
+var panelModel 
+gltfLoader.load(
+    'models/panel/panel.glb',
+    (gltf) =>
+    {
+        panelModel = gltf.scene
+
+        panelModel.traverse((child) => 
+        {
+            child.material = panelMaterial
+        })
+
+        panelModel.scale.set(0.13 * (sizes.width / sizes.height), 0.13 * (sizes.width / sizes.height), 0.13 * (sizes.width / sizes.height))
+        panelModel.position.y = -0.5
+        panelModel.rotation.set(1.266, 0.078, -0.414)
+
+        scenePanel.add(panelModel)
+
+        // gui.add(panelModel.rotation, 'y').min(- Math.PI).max(Math.PI).step(0.001).name('rotation y panel')
+        // gui.add(panelModel.rotation, 'x').min(- Math.PI).max(Math.PI).step(0.001).name('rotation x panel')
+        // gui.add(panelModel.rotation, 'z').min(- Math.PI).max(Math.PI).step(0.001).name('rotation z panel')
+    }
+)
+
 var moduleModel
 gltfLoader.load(
     'models/module/module.glb',
@@ -71,21 +104,13 @@ gltfLoader.load(
         moduleModel.scale.set(0.3 * (sizes.width / sizes.height), 0.3 * (sizes.width / sizes.height), 0.3 * (sizes.width / sizes.height))
         moduleModel.rotation.set(0.16, 0.4, 0)
 
-        scene.add(moduleModel)
+        sceneModule.add(moduleModel)
 
         // gui.add(moduleModel.rotation, 'x').min(- Math.PI).max(Math.PI).step(0.001).name('rotation x model')
         // gui.add(moduleModel.rotation, 'y').min(- Math.PI).max(Math.PI).step(0.001).name('rotation y model')
         // gui.add(moduleModel.rotation, 'z').min(- Math.PI).max(Math.PI).step(0.001).name('rotation z model')
     }
 )
-
-/**
- * Lights
- */
-// const directionalLight = new THREE.DirectionalLight('#ffffff', 2)
-// directionalLight.position.set(1, 1, 0)
-// scene.add(directionalLight)
-
 /**
  * Sizes
  */
@@ -98,18 +123,25 @@ window.addEventListener('resize', () =>
 {
     // Update models
     moduleModel.scale.set(0.3 * (sizes.width / sizes.height), 0.3 * (sizes.width / sizes.height), 0.3 * (sizes.width / sizes.height))
+    panelModel.scale.set(0.14 * (sizes.width / sizes.height), 0.14 * (sizes.width / sizes.height), 0.14 * (sizes.width / sizes.height))
 
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+    // Update cameras
+    cameraPanel.aspect = sizes.width / sizes.height
+    cameraPanel.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    cameraModule.aspect = sizes.width / sizes.height
+    cameraModule.updateProjectionMatrix()
+
+    // Update renderers
+    rendererPanel.setSize(sizes.width, sizes.height)
+    rendererPanel.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+    rendererModule.setSize(sizes.width, sizes.height)
+    rendererModule.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 
@@ -117,43 +149,64 @@ window.addEventListener('resize', () =>
  * Camera
  */
 // Group
-const cameraGroup = new THREE.Group()
-scene.add(cameraGroup)
+// const cameraGroup = new THREE.Group()
+// scene.add(cameraGroup)
 
 // Base camera
-const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 6
-cameraGroup.add(camera)
+const cameraPanel = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+cameraPanel.position.z = 6
+// cameraGroup.add(cameraPanel)
+
+const cameraModule = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+cameraModule.position.z = 6
+// cameraGroup.add(cameraModule)
+
 
 // controls
-const controls = new OrbitControls(camera, canvas)
-controls.target.set(0, 0, 0)
-controls.enableDamping = true
-controls.enableZoom = false
+const controlsPanel = new OrbitControls(cameraPanel, canvasPanel)
+controlsPanel.target.set(0, 0, 0)
+controlsPanel.enableDamping = true
+controlsPanel.enableZoom = false
+
+const controlsModule = new OrbitControls(cameraModule, canvasModule)
+controlsModule.target.set(0, 0, 0)
+controlsModule.enableDamping = true
+controlsModule.enableZoom = false
 
 /**
  * Renderer
  */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
+const rendererPanel = new THREE.WebGLRenderer({
+    canvas: canvasPanel,
     alpha: true,
     antialias: true
 })
-renderer.useLegacyLights = false
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-// renderer.outputColorSpace = THREE.SRGBColorSpace;
-renderer.outputColorSpace = THREE.LinearSRGBColorSpace
+rendererPanel.useLegacyLights = false
+rendererPanel.setSize(sizes.width, sizes.height)
+rendererPanel.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+// rendererPanel.outputColorSpace = THREE.SRGBColorSpace;
+rendererPanel.outputColorSpace = THREE.LinearSRGBColorSpace
+
+const rendererModule = new THREE.WebGLRenderer({
+    canvas: canvasModule,
+    alpha: true,
+    antialias: true
+})
+rendererModule.useLegacyLights = false
+rendererModule.setSize(sizes.width, sizes.height)
+rendererModule.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+// rendererModule.outputColorSpace = THREE.SRGBColorSpace;
+rendererModule.outputColorSpace = THREE.LinearSRGBColorSpace
 
 
-gui.add(renderer, 'toneMapping', {
+gui.add(rendererPanel, 'toneMapping', {
     No: THREE.NoToneMapping,
     Linear: THREE.LinearToneMapping,
     Rainhard: THREE.ReinhardToneMapping,
     Cineon: THREE.CineonToneMapping,
     ACESFilmic: THREE.ACESFilmicToneMapping
 })
-gui.add(renderer, 'toneMappingExposure').min(0).max(10).step(0.001)
+gui.add(rendererPanel, 'toneMappingExposure').min(0).max(10).step(0.001)
 
 
 /**
@@ -195,25 +248,28 @@ window.addEventListener('mousemove', (event) =>
 /**
  * Animate
  */
-const clock = new THREE.Clock()
-let previousTime = 0
+// const clock = new THREE.Clock()
+// let previousTime = 0
 
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - previousTime
-    previousTime = elapsedTime
+    // const elapsedTime = clock.getElapsedTime()
+    // const deltaTime = elapsedTime - previousTime
+    // previousTime = elapsedTime
 
-    const parallaxX = cursor.x * 0.05
-    const parallaxY = - cursor.y * 0.05
-    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
-    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
+    // const parallaxX = cursor.x * 0.05
+    // const parallaxY = - cursor.y * 0.05
+    // cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
+    // cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
     
     // Update controlls
-    controls.update()
+    controlsPanel.update()
+    controlsModule.update()
 
     // Render
-    renderer.render(scene, camera)
+    rendererPanel.render(scenePanel, cameraPanel)
+    rendererModule.render(sceneModule, cameraModule)
+
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
