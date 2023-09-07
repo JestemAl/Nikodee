997472c84026f0978874e21e3d52d07e1c20bd5f
colbery <92644519+colbery@users.noreply.github.com>
2023-09-07 22:59:48 +0200

    productCard

--- src/script.js
+++ src/script.js
@@ -50,21 +50,21 @@ const handleOnMove = e => {
 
 // const handleOnMove = e => {
 //   if (track.dataset.mouseDownAt === "0") return;
-  
+
 //   const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
 //   const maxDelta = parseFloat(track.dataset.maxDelta); // Pobierz maksymalne przesunięcie z dataset
 
-  
+
 //   const percentage = (mouseDelta / maxDelta) * -100,
 //         nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
 //         nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
-  
+
 //   track.dataset.percentage = nextPercentage;
-  
+
 //   track.animate({
 //     transform: `translate(${nextPercentage}%, -50%)`
 //   }, { duration: 1200, fill: "forwards" });
-  
+
 //   for(const image of track.getElementsByClassName("image")) {
 //     image.animate({
 //       objectPosition: `${100 + nextPercentage}% center`
@@ -120,24 +120,42 @@ function toggleClass(element, className, add) {
   }
 }
 
-/**
- * order.html
- */
+document.addEventListener("DOMContentLoaded", function () {
+  let colorSpans = document.querySelectorAll(".product-colors span");
+  let h2Element = document.querySelector(".container .details .content h2");
 
-document.addEventListener("DOMContentLoaded", function() {
-  // Pobierz wszystkie elementy .box
-  const boxes = document.querySelectorAll(".box");
-
-  // Dodaj obsługę kliknięcia dla każdego elementu .box
-  boxes.forEach(function(box) {
-      box.addEventListener("click", function() {
-          // Usuń klasę "active" z wszystkich elementów .box
-          boxes.forEach(function(box) {
-              box.classList.remove("active");
-          });
-
-          // Dodaj klasę "active" tylko do klikniętego elementu .box
-          this.classList.add("active");
+  colorSpans.forEach(function (span) {
+    span.addEventListener("click", function () {
+      // Usuń klasę "active" ze wszystkich elementów <span> z klasą "product-colors"
+      colorSpans.forEach(function (el) {
+        el.classList.remove("active");
       });
+
+      // Dodaj klasę "active" do aktualnie klikniętego elementu <span>
+      this.classList.add("active");
+      var dataSet = this.getAttribute("data-set");
+
+      // Ustaw tekst w elemencie h2 na podstawie wartości data-set
+      h2Element.innerHTML = "Zestaw " + dataSet;
+
+      // Pobierz wartości atrybutów data-color-sec, data-color-primary i data-pic z klikniętego elementu <span>
+      var dataColorSec = this.getAttribute("data-color-sec");
+      var dataColorPrimary = this.getAttribute("data-color-primary");
+      var dataPic = this.getAttribute("data-pic");
+
+      // Ustaw style i źródło obrazka na podstawie pobranych wartości
+      document.body.style.background = dataColorPrimary;
+      document.querySelector(".content h2").style.color = dataColorSec;
+      document.querySelector(".content h3").style.color = dataColorSec;
+      document.querySelector(".container .imgBx").style.background = dataColorSec;
+      document.querySelector(".container .details button").style.background = dataColorSec;
+      document.querySelector(".imgBx img").src = dataPic;
+
+      // Zaktualizuj pozostałe elementy
+      var spanElement = document.querySelector(".container .details .content .span");
+      var pElement = document.querySelector(".container .details .content .description");
+      spanElement.innerHTML = "Zestaw " + dataSet + " Zestaw " + dataSet;
+      //pElement.textContent = "Zestaw"
+    });
   });
 });
