e6dd17afd1ee097317c754250102e989e1ec247a
colbery <92644519+colbery@users.noreply.github.com>
2023-09-08 19:38:54 +0200

    fixed: [slider_1page_didntwork], feature: [hamburgermenu,mobilemenu]

--- src/script.js
+++ src/script.js
@@ -100,3 +100,60 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+const tabs = document.querySelectorAll(".tab");
+const panels = document.querySelectorAll(".panel");
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const logo = document.getElementById("logo");
+
+//tabs nasluchiwanie
+tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
+panels.forEach((panel) => panel.addEventListener("click", closeNav));
+
+//ham button listener
+btn.addEventListener("click", navToggle);
+
+function onTabClick(e) {
+  //wylacz bordery
+  tabs.forEach((tab) => {
+    tab.children[0].classList.remove(
+      "border-softRed",
+      "border-b-4",
+      "md:border-b-0"
+    );
+  });
+
+  //schowaj wszystyko
+  panels.forEach((panel) => {
+    panel.classList.add("hidden");
+  });
+
+  //aktywujemy tab i panel ktory klikniecieee
+  e.target.classList.add("border-softRed", "border-b-4");
+  const classString = e.target.getAttribute("data-target");
+  document
+    .getElementById("panels")
+    .getElementsByClassName(classString)[0]
+    .classList.remove("hidden");
+}
+
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+
+  if (menu.classList.contains("flex")) {
+    logo.setAttribute("src", "./images/logo-bookmark-footer.svg");
+  } else {
+    logo.setAttribute("src", "./images/logo-bookmark.svg");
+  }
+}
+
+function closeNav() {
+  setTimeout(() => {
+    btn.classList.toggle("open");
+    menu.classList.toggle("flex");
+    menu.classList.toggle("hidden");
+  }, 500);
+}
